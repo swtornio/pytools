a9252db015227d57efa4a4f3f35c4e6a05af7cf7
swtornio <swtornio@gmail.com>
2021-10-16 18:11:48 -0500

    minimum viable expert

--- project1/scoper.py
+++ project1/scoper.py
@@ -10,3 +10,35 @@
 # IP addresses and CIDR notation, having it process it both types.
 
 # https://ipgeolocation.io/documentation/ip-geolocation-api.html
+
+import requests
+import configparser
+import ipaddress
+
+# store the API key in an external file and make sure to add the file
+# to .gitignore
+
+cfg = configparser.ConfigParser()
+cfg.read('ipgeo.cfg')
+
+IPGEO_KEY = cfg.get('KEYS', 'api_key', raw='')
+IPGEO_URL = "https://api.ipgeolocation.io/ipgeo"
+
+def locate(ip):
+    '''Query IP Geo database for given IP and print Owner'''
+    resp = requests.get(f'{IPGEO_URL}?apiKey={IPGEO_KEY}&ip={ip}')
+    location_info = resp.json()
+    print(f'{ip} is owned by {location_info["isp"]}, located in '
+          f'{location_info["city"]}, {location_info["state_prov"]}.')
+
+
+if __name__ == '__main__':
+    with open("targets.txt", "r") as f:
+        targets = [line.strip() for line in f.readlines()]
+        for target in targets:
+            # check if it's an IP or a CIDR
+            if "/" in target:
+                for addr in ipaddress.ip_network(target).hosts():
+                    locate(addr)
+            else:
+               locate(target)
